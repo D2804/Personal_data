07e102644d7057c19e85d4fcbc17060901aa732a
Lenovo <Lenovo@DESKTOP-RC4DNGU.bbrouter>
2022-10-11 12:05:52 +0530

    committing first time

---
+++ src/basicProgam/Program1.java
@@ -0,0 +1,12 @@
+package basicProgam;
+
+public class Program1 {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println(" Hii this is program 1");
+	}
+}
